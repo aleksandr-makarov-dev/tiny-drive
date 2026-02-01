8d3f79255c3460ed3e44badb1c08db058e9a6c78
Aleksandr Makarov <alexandr.makarov.2000@gmail.com>
2026-02-01 14:30:50 +0200

    chore: Add Tag

--- TinyDrive.Features/Features/Nodes/CreateFolder/CreateFolderEndpoint.cs
+++ TinyDrive.Features/Features/Nodes/CreateFolder/CreateFolderEndpoint.cs
@@ -1,6 +1,7 @@
 ﻿using Carter;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using TinyDrive.Features.Common.Constants;
 using TinyDrive.Features.Common.Extensions;
 
 namespace TinyDrive.Features.Features.Nodes.CreateFolder;
@@ -10,7 +11,8 @@ public sealed class CreateFolderEndpoint : ICarterModule
 
 	public void AddRoutes(IEndpointRouteBuilder app)
 	{
-		app.MapPost("api/nodes", HandleAsync);
+		app.MapPost("api/nodes", HandleAsync)
+			.WithTags(Tags.Nodes);
 	}
 
 	private static async Task<IResult> HandleAsync([FromBody] CreateFolderRequest request, ISender sender)
